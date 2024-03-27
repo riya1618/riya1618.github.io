9272411ae5440331956582b4f1eea37701e7737a
riya1618 <riyasandeep16@gmail.com>
2024-03-27 23:54:27 +0530

    Initial Commit

---
+++ src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Welcome to my front-end developer portfolio! With a passion for crafting engaging user experiences, I specialize in HTML, CSS, and JavaScript to create stunning, interactive websites and applications. Explore my projects to see how I blend creativity with technical expertise to deliver visually captivating and user-friendly digital experiences. From responsive designs to dynamic animations, I bring innovation and precision to every project, ensuring a seamless journey for users. Let's build something incredible together",
+		keywords: ["Riya", "Riya S", "Riya salvi"],
+	},
+
+	{
+		page: "about",
+		description:
+			"Passionate front-end developer with a knack for crafting intuitive user experiences and visually captivating interfaces. My portfolio showcases a diverse range of front-end projects, each highlighting my expertise in HTML, CSS, and JavaScript. From responsive web designs to interactive applications, my work emphasizes clean code, attention to detail, and a keen eye for design. Whether it's creating dynamic animations, implementing user-friendly navigation, or optimizing for performance, my front-end projects consistently prioritize usability and aesthetics to deliver impactful digital experiences",
+		keywords: ["Riya", "Riya S", "Riya salvi"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"Passionate front-end developer with a knack for crafting intuitive user experiences and visually captivating interfaces. My portfolio showcases a diverse range of front-end projects, each highlighting my expertise in HTML, CSS, and JavaScript. From responsive web designs to interactive applications, my work emphasizes clean code, attention to detail, and a keen eye for design. Whether it's creating dynamic animations, implementing user-friendly navigation, or optimizing for performance, my front-end projects consistently prioritize usability and aesthetics to deliver impactful digital experiences",
+		keywords: ["Riya", "Riya S", "Riya salvi"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Riya", "Riya S", "Riya salvi"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,61 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Riya ",
+		name: "Riya Salvi.",
+		email: "riyasandeep16@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/RiyaSalvi02",
+		github: "https://github.com/riya1618",
+		linkedin: "https://www.linkedin.com/in/riyasalvi1618/",
+		instagram: "https://www.instagram.com/salvi_riya16/",
+		stackoverflow: "https://stackoverflow.com/users/23485508/riya-salvi",
+		facebook: "https://www.facebook.com/profile.php?id=100078927284647",
+	},
+
+	homepage: {
+		title: "Full-stack web developer",
+		description:
+			"My name is Riya Salvi , I'm a front-end developer based in Pune, Maharashtra,India. Welcome to my front-end developer portfolio! With a passion for crafting engaging user experiences, I specialize in HTML, CSS, and JavaScript to create stunning, interactive websites and applications. Explore my projects to see how I blend creativity with technical expertise to deliver visually captivating and user-friendly digital experiences. From responsive designs to dynamic animations, I bring innovation and precision to every project, ensuring a seamless journey for users. Let's build something incredible together ",
+	},
+
+	about: {
+		title: "I’m Riya S. I live in Pune, where I design the future.",
+		description:
+			"Passionate front-end developer with a knack for crafting intuitive user experiences and visually captivating interfaces. My portfolio showcases a diverse range of front-end projects, each highlighting my expertise in HTML, CSS, and JavaScript. From responsive web designs to interactive applications, my work emphasizes clean code, attention to detail, and a keen eye for design. Whether it's creating dynamic animations, implementing user-friendly navigation, or optimizing for performance, my front-end projects consistently prioritize usability and aesthetics to deliver impactful digital experiences",
+	},
+
+	projects: [
+		{
+			title: "tic-tac-toe ",
+			description:
+				"Tic Tac Toe is a classic game implemented using JavaScript, HTML, and CSS. This project showcases my proficiency in front-end web development and JavaScript programming. It features a responsive and interactive user interface where players can engage in a timeless game of Tic Tac Toe. ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/riya1618/tic-tac-toe-js",
+		},
+
+		{
+			title: "Digital Clock",
+			description:
+				"Digital Clock JS Project A sleek and efficient JavaScript project showcasing my proficiency in web development. This project features a digital clock interface built entirely with JavaScript, HTML, and CSS. It demonstrates my ability to manipulate the DOM (Document Object Model) to create dynamic and responsive user interfaces.",
+				logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/riya1618/CODSOFT/tree/main/Digital%20clock",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+	],
+};
+
+export default INFO;
